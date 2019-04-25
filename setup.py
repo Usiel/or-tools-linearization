53b43811a945103cfb0e449b6c5f72c034a185df
Usiel Riedl <usiel@dahmakan.com>
2019-04-25 13:51:14 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='or-tools-linearization',
+    version='0.1.0',
+    description='This package allows you to linearize non-linear functions and make use of them with or-tools in '
+                'constraints or the objective function',
+    author='Usiel Riedl',
+    author_email='usiel.riedl@gmail.com',
+)
